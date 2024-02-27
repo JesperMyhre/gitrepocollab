e93bd899ca2ea730614c806251ccb748be5eb39e
AnneOmberg <111729606+AnneOmberg@users.noreply.github.com>
2024-02-27 14:51:32 +0100

    wer
    sdfg

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import "./App.css";
 function App() {
   return (
     <>
-      <h1>Jesper sin side som anne ødlegger</h1>
+      <h1>Jesper sin side som Anne ødlegger</h1>
       <p>denne fuck dayum paragrafen har jeg skrevet</p>
       <p>Anne prøver...</p>
     </>
