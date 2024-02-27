f8584a0228a504158bf234c4a33dda9c750d74b3
JesperMyhre <111729406+JesperMyhre@users.noreply.github.com>
2024-02-27 15:00:29 +0100

    endringer

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
-import './App.css'
+import "./App.css";
 
 function App() {
-
   return (
     <>
-      <h1>Jesper sin side som Anne ødlegger</h1>
-      <p>denne fuck dayum paragrafen har jeg skrevet</p>
-      <p>Anne prøver...</p>
+      <h1>Git Repo Collab</h1>
+      <p>Dette er en paragraf, den har jeg skrevet</p>
     </>
   );
 }
